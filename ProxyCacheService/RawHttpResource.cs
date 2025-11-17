c765d371e162179cf586dc3b56be91c225bcbd6a
sabrinalamghari <sabrinalmghari@gmail.com>
2025-11-17 10:14:57 +0100

    refactor proxy pour les appels

---
+++ ProxyCacheService/RawHttpResource.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Net.Http;
+
+namespace ProxyCacheService
+{
+    public class RawHttpResource
+    {
+        public string Url { get; }
+        public string Content { get; }
+
+        public RawHttpResource(string url)
+        {
+            Url = url;
+
+            using (var client = new HttpClient())
+            {
+                Console.WriteLine("[Proxy] HTTP GET " + url);
+
+                var response = client.GetAsync(url).Result;
+                response.EnsureSuccessStatusCode();
+
+                Content = response.Content.ReadAsStringAsync().Result;
+            }
+        }
+    }
+}
